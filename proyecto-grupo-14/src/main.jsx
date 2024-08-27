321981a08d4ac7a0cc21106c6f9a8f59070ab993
leandroace <acevedo.leandro@correounivalle.edu.co>
2024-08-27 14:50:53 -0500

    Modify main.jsx

--- proyecto-grupo-14/src/main.jsx
+++ proyecto-grupo-14/src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/Quiz",
+    element: <Quiz />
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
