3dbbb20c618c3ef4ba52b2242ae2d0b51c7865d4
Andres <124212022+AndresIsCoding@users.noreply.github.com>
2024-10-28 11:15:20 -0500

    Fixed header

--- proyecto-grupo-14/src/components/Header.jsx
+++ proyecto-grupo-14/src/components/Header.jsx
@@ -4,31 +4,18 @@ import '../styles/Header.css'
 import Navbar from './Navbar';
 
 const Header = () => {
-  const [logo, setLogo] = useState("../images/Logo.png");
-  const [textoColor, setTextColor] = useState("#000")
-  const location = useLocation();
-  
-  useEffect(()=>{
-    if(location.pathname === "/inicio"){
-        setLogo("../images/DarkLogo.png");
-        setTextColor("#000");
-    } else{
-        setLogo("../images/Logo.png");
-        setTextColor("#FFFFFF");
-    }
-    }, [location]);
   return (
     <>
       <Navbar/>
       <header>
         <div className="logo">
-          <img src={logo} alt="logo" />
+          <img src="../images/DarkLogo.png" alt="logo" />
         </div>
         <nav>
-          <Link to="/inicio" style={{color:textoColor}}>Inicio</Link>
-          <Link to="/contaminacion" style={{color:textoColor}}>Contaminación</Link>
-          <Link to="/escasez"  style={{color:textoColor}}>Escasez</Link>
-          <Link to="/acidificacion"  style={{color:textoColor}}>Acidificación</Link>
+          <Link to="/inicio">Inicio</Link>
+          <Link to="/contaminacion">Contaminación</Link>
+          <Link to="/escasez">Escasez</Link>
+          <Link to="/acidificacion">Acidificación</Link>
         </nav>
       </header>
     </>
