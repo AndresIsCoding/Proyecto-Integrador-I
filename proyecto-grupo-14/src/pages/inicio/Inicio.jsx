fddca24d9d20948c42bd9c79b975ede76c72a1b9
Andres <124212022+AndresIsCoding@users.noreply.github.com>
2024-10-28 00:57:29 -0500

    Snapshot 10 28

--- proyecto-grupo-14/src/pages/inicio/Inicio.jsx
+++ proyecto-grupo-14/src/pages/inicio/Inicio.jsx
@@ -12,23 +12,35 @@ const Inicio = () => {
   return (
     <div className="main-container">
       <Header /> 
+
       <Canvas camera={cameraSettings}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[10, 10, 5]} intensity={1} />
         <Earth position={[0, 0, 0]}/>
       </Canvas>
-      <group className="title1">
-        <h2 className="titleA">Menos del 1% es apta para el consumo</h2>
-      </group>
-      <group className="title2">
-        <h2 className="titleB">Aunque cubra el 70%</h2>
-      </group>
-      <div>
+
+      <div className="main-title">
+        <h1>Solo podemos <br/>beber menos del <br/>1%</h1>
+      </div>
+
+      <div className="title1">
+        <h2>Aunque cubra el 70% del planeta...</h2>
+      </div>
+
+      <div className="small-text">
+        <h2 className="title3">¿Porque saber esto es importante?</h2>
         <p className="paragraph">
-          Este es un párrafo que proporciona información adicional sobre el tema tratado en la página.
-          Aquí hay más información en una nueva línea.
+          Se estima que para 2050 aproximadamente 6000 millones<br/> de personas
+          sufrirán de escasez de agua debido<br/>al aumento de los niveles de contaminación.
         </p>
       </div>
+
+      <div className="second-container">
+        <group className="title2">
+          <h2>Pero ese no es el único problema</h2>
+        </group>
+      </div>
+
     </div>
     
   );
