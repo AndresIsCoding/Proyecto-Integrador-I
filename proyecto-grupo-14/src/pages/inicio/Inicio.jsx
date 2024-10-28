d8c40d746b24f80f4136ec869d4e0cc007f11866
Andres <124212022+AndresIsCoding@users.noreply.github.com>
2024-10-27 20:06:43 -0500

    Snapshot 10 27 B
    Text added to inicio

--- proyecto-grupo-14/src/pages/inicio/Inicio.jsx
+++ proyecto-grupo-14/src/pages/inicio/Inicio.jsx
@@ -1,9 +1,8 @@
-// Inicio.jsx
 import { Canvas } from "@react-three/fiber";
 import './Inicio.css';
 import Header from "../../components/Header.jsx";
-import OilBarrel from "../contaminacion/OilBarrel.jsx";
 import Earth from "./Earth.jsx";
+import { Text } from "@react-three/drei";
 
 const Inicio = () => {
   const cameraSettings = { 
@@ -11,15 +10,27 @@ const Inicio = () => {
   };
 
   return (
-    <div className="inicio-background">
+    <div className="main-container">
       <Header /> 
-
       <Canvas camera={cameraSettings}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[10, 10, 5]} intensity={1} />
         <Earth position={[0, 0, 0]}/>
       </Canvas>
+      <group className="title1">
+        <h2 className="titleA">Menos del 1% es apta para el consumo</h2>
+      </group>
+      <group className="title2">
+        <h2 className="titleB">Aunque cubra el 70%</h2>
+      </group>
+      <div>
+        <p className="paragraph">
+          Este es un párrafo que proporciona información adicional sobre el tema tratado en la página.
+          Aquí hay más información en una nueva línea.
+        </p>
+      </div>
     </div>
+    
   );
 };
 
