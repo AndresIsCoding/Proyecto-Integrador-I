696760cc23a55b9d8796fea69292b0e0675e2ffe
WiloxW <62610100+WiloxW@users.noreply.github.com>
2024-11-10 17:16:41 -0500

    added text of sensibilization, css also edited

--- proyecto-grupo-14/src/pages/acidificacion/Sensibilizacion/elements/SensesText.jsx
+++ proyecto-grupo-14/src/pages/acidificacion/Sensibilizacion/elements/SensesText.jsx
@@ -1,12 +1,15 @@
 import { Html } from "@react-three/drei";
+import '../AcidificacionSen.css';
 
 const SensesText = () =>{
 return(
     <Html>
-     <h1>Sensibilizacion</h1>
-     <p>
-       texto de prueba
-     </p>
+     <div className="acidificacion-intro">
+      <h1>Sensibilizacion</h1>
+      <p>
+      indagando en las consecuencias de este problema, los peces al encontrarse en un medio más ácido de lo normal gastan más energía para sus funciones fisiológicas, reduciendo su población. Se terminará reduciendo la disponibilidad de carbonato para varias especies de mariscos para formar sus esqueletos protectores y conchas. También, el más conocido es el cómo se ven afectados los arrecifes de coral, despojando al 25 por ciento de las especies marina de refugio necesario para su supervivencia.
+      </p>
+     </div>
     </Html>
 )
 }
