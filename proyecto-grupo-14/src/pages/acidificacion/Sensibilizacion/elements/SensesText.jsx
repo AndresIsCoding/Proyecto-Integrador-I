67310f9006dee85d8dee64ece35735bb2bdf31cc
WiloxW <62610100+WiloxW@users.noreply.github.com>
2024-11-10 19:19:04 -0500

    fixed css sensibilization

--- proyecto-grupo-14/src/pages/acidificacion/Sensibilizacion/elements/SensesText.jsx
+++ proyecto-grupo-14/src/pages/acidificacion/Sensibilizacion/elements/SensesText.jsx
@@ -4,7 +4,7 @@ import '../AcidificacionSen.css';
 const SensesText = () =>{
 return(
     <Html>
-     <div className="acidificacion-intro">
+     <div className="acidificacionsen-intro">
       <h1>Sensibilizacion</h1>
       <p>
       indagando en las consecuencias de este problema, los peces al encontrarse en un medio más ácido de lo normal gastan más energía para sus funciones fisiológicas, reduciendo su población. Se terminará reduciendo la disponibilidad de carbonato para varias especies de mariscos para formar sus esqueletos protectores y conchas. También, el más conocido es el cómo se ven afectados los arrecifes de coral, despojando al 25 por ciento de las especies marina de refugio necesario para su supervivencia.
